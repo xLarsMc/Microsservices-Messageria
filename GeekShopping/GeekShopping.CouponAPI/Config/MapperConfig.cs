82e003e1762d7245a6d94af9d767c7e8f7fa0589
Leandro Henrique <leandrohenriquetti@gmail.com>
2025-10-23 11:44:51 -0300

    Finalizado cupom

---
+++ GeekShopping/GeekShopping.CouponAPI/Config/MapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using GeekShopping.CouponAPI.Data.DTO;
+using GeekShopping.CouponAPI.Model;
+
+namespace GeekShopping.CouponAPI.Config
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            RegisterMaps();
+        }
+
+        private void RegisterMaps()
+        {
+            CreateMap<Coupon, CouponDTO>().ReverseMap();
+        }
+    }
+}
